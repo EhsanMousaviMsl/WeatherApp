393e4b7ff2d3d7cd3be150138a9d2d32b8070484
EhsanMousaviMsl <ehsanmousavi7472@yahoo.com>
2025-07-19 16:34:36 +0300

    Initial Commit

---
+++ SwiftUI_WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI_WeatherApp
+//
+//  Created by Yahya Goorakani on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
